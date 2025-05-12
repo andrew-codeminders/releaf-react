58fba02942fa6a7c49cc0448ae9b755664810aa4
Andrew <akondratiyk@codeminders.com>
2025-05-12 18:52:10 +0300

    feat: hardcoded order

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,24 +1,29 @@
-import { HustlepayPaymentGateway } from "@hustlepay/payment-gateway";
-import "@hustlepay/payment-gateway/dist/index.css";
-import React from "react";
-
+import { HustlepayPaymentGateway } from '@hustlepay/payment-gateway';
+import '@hustlepay/payment-gateway/dist/index.css';
+import React from 'react';
+import { CustomTrigger } from '../components/CustomTrigger';
 
 const order = {
   customer_id: -1,
   order_id: 1,
+  full_name: 'John Doe',
+  email: 'test@example.com',
+  amount: 9998,
   order_items: [
+    {
+      name: 'Muscles & Joints Extra Strength Cream (XR)',
+      price: 49.99,
+      quantity: 2,
+      image: '/assets/images/ReLeaf.webp',
+    },
   ],
-  full_name: "John Doe",
-  email: "test@example.com",
-  amount: 2600, // OPTIONALLY: retrieve order amount from the site
-  billing_address_1: "",
-  billing_address_2: "",
-  redirect_url: "http://localhost:8080/?wc-api=hustlepay_payment_processed", // TODO: implement the callback endpoint
+  billing_address_1: '',
+  billing_address_2: '',
+  redirect_url: 'http://localhost:8080/?wc-api=hustlepay_payment_processed',
 };
 
 const config = {
-  pluginUrl:
-    "./", // TODO: replace with CDN url in the future, for now we get images from public assets
+  pluginUrl: './', // TODO: replace with CDN url in the future, for now we get images from public assets
   publicKey: process.env.PUBLIC_KEY,
   secretKey: process.env.SECRET_KEY,
   stripeConfig: {
